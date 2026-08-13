a48a92baeb1efe93fafb16b78e963559bfba692e
ozcarprez <107212335+ozcarprez@users.noreply.github.com>
2026-08-13 12:53:16 -0700

    Delete nuvi-app-codigo directory

--- nuvi-app-codigo/lib/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://ztsxvfmlsvblkwzzuple.supabase.co';
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_k9lf0ekCqr65ceifYB6onQ_nN_XiBDJ';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
